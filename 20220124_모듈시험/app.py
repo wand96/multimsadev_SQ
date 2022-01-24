ba3d70f1ae29d92c7a8028b619e26a47122b2694
wand96 <84069743+wand96@users.noreply.github.com>
2022-01-24 18:35:13 +0900

    Add files via upload

---
+++ 20220124_모듈시험/app.py
@@ -0,0 +1,38 @@
+# pip install flask 설치 진행
+# API 요청 할 pip install requests
+
+from flask import Flask
+import json
+import requests
+
+
+# 변수 선언 - 프로그램의 이름 저장하는 변수(파일 이름 저장 변수)
+# application server 개발 app 변수를 많이 사용
+app = Flask(__name__) # flask 프로그램 시작 기본 값은 = app.py 파일을 생성
+
+#함수 선언 
+# 시작할 때 경로(route) 선언해야함
+@app.route("/") # 웹 사이트 경로를 지정 - 앞에서 선언한 app 변수를 사용
+def FlaskLab(): # 요청 - 메서드(함수) 이름 요청에서 사용하는 것
+    return "Flask 데이터 응답" # 응답 -  return 에서 돌려주는 데이터가 응답
+
+@app.route("/data1")
+def FlaskData():#startPage, pageCount, address): # 요청 받음
+    keyValue = r"XegNwpglw%2BOi95q%2FIdeoFlBalw%2F%2Bh4xL8v9Pcxyqk2VKvY9hQrA2MxJM5HxhImy9HfP%2B0vPgE6rp6nQlj9iScQ%3D%3D"   
+
+    dataURL = "https://api.odcloud.kr/api/apnmOrg/v1/list?"
+    dataURL += "page=" + str(1) + "&perPage=" + str(10)
+    dataURL += "&cond=" + "강남구" # 또는 본인 사는 지역구
+    dataURL += "&serviceKey=" + keyValue
+    
+    #tempURL = r"https://api.odcloud.kr/api/apnmOrg/v1/list?page=1&perPage=10
+    # &cond=""
+    # &serviceKey=""
+    # https://api.odcloud.kr/api/apnmOrg/v1/list?페이지 그리고 인증 값 연결
+    
+    dataResult = requests.get(dataURL)
+    #공공데이터 요청 후 데이터 받기 : flask - request / requests 기능 사용"
+
+    return json.loads(dataResult)   # 공공데이터 결과 값 응답
+
+
